362519e95ad39c7a4628dcc74ab0ba011986bd15
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 01:37:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:yukihiro_matsumoto] = {}
+	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash   
     
 end
 
